6e9d1940a1b95e395eebe2a52db793c43f600226
Felipe <felipemachadocosta806@gmail.com>
2021-05-15 16:42:43 -0300

    ajuste

--- src/database/migrations/1620954220228-CreateUsers.ts
+++ src/database/migrations/1620954220228-CreateUsers.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm"; 
 
 export class CreateUsers1620954220228 implements MigrationInterface {
 
